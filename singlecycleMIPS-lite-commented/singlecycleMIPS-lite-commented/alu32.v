d028887d377fabe8c4814c316c721163c31a4ac5
Muhammed Karagul <talhakaragul@marun.edu.tr>
2024-05-22 23:01:43 +0300

    alu32 operations are done

--- singlecycleMIPS-lite-commented/singlecycleMIPS-lite-commented/alu32.v
+++ singlecycleMIPS-lite-commented/singlecycleMIPS-lite-commented/alu32.v
@@ -1,24 +1,33 @@
 module alu32(sum,a,b,zout,gin);//ALU operation according to the ALU control line values
+
 output [31:0] sum;
+
 input [31:0] a,b; 
-input [2:0] gin;//ALU control line
+input [3:0] gin;//ALU control line
+
 reg [31:0] sum;
 reg [31:0] less;
+
 output zout;
 reg zout;
 always @(a or b or gin)
 begin
-	case(gin)
-	3'b010: sum=a+b; 		//ALU control line=010, ADD
-	3'b110: sum=a+1+(~b);	//ALU control line=110, SUB
-	3'b111: begin less=a+1+(~b);	//ALU control line=111, set on less than
-			if (less[31]) sum=1;	
-			else sum=0;
-		  end
-	3'b000: sum=a & b;	//ALU control line=000, AND
-	3'b001: sum=a|b;		//ALU control line=001, OR
-	default: sum=31'bx;	
-	endcase
+    case(gin)
+    4'b0010: sum=a+b;         //ALU control line=0010, ADD
+    4'b0110: sum=a+1+(~b);    //ALU control line=0110, SUB
+    4'b0111: begin less=a+1+(~b);    //ALU control line=0111, set on less than
+            if (less[31]) sum=1;
+            else sum=0;
+          end
+    4'b0000: sum=a & b;    //ALU control line=0000, AND 
+    4'b0001: sum=a|b;        //ALU control line=0001, OR
+    4'b1010: sum=~(a | b) // ALU control line=1010, NOR	- nori
+    4'b1001: sum=a^b;        //ALU control line=1001, XOR - jmxor
+	4'b1000: sum=a           //ALU control line=1000, BRV
+    default: sum=31'bx;
+    endcase
 zout=~(|sum);
 end
 endmodule
+
+//blezal olayı sikintili
