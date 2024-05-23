26469e999f1040695854754a25a588f36e558301
0xEnsar <ensaryozgat@gmail.com>
2024-05-23 23:19:38 +0300

    init update

--- singlecycleMIPS-lite-commented/singlecycleMIPS-lite-commented/processor.v
+++ singlecycleMIPS-lite-commented/singlecycleMIPS-lite-commented/processor.v
@@ -178,7 +178,7 @@ signext sext(instruc[15:0],extad);
 zeroext zext(instruc[15:0],zextad);
 
 //ALU control unit
-alucont acont(aluop1,aluop0,instruc[3],instruc[2], instruc[1], instruc[0] ,gout);
+alucont acont(aluop1,aluop0,instruc[5],instruc[4],instruc[3],instruc[2], instruc[1], instruc[0] ,gout);
 
 //Shift-left 2 unit
 shift shift2(sextad,extad);
